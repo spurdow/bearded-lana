6f9879f305d6c27e9770ca56232356d3bdca77aa
unknown <montecillodavid@gmail.com>
2014-10-20 00:50:33 +0800

    s

---
+++ app/src/main/java/com/app/belive/ActionBarTester.java
@@ -0,0 +1,10 @@
+package com.app.belive;
+
+import com.app.parent.ParentActionBarPullToRefresh;
+
+/**
+ * Created by David Montecillo on 10/20/2014.
+ */
+public class ActionBarTester extends ParentActionBarPullToRefresh{
+    
+}
--- app/src/main/java/com/app/belive/Causes.java
+++ app/src/main/java/com/app/belive/Causes.java
@@ -1,10 +1,23 @@
 package com.app.belive;
 
+import android.view.View;
+
 import com.app.parent.ParentActionBar;
 import com.app.parent.ParentActionBarPullToRefresh;
+import com.app.parent.ParentFragment;
 
 /**
  * Created by David Montecillo on 10/20/2014.
  */
-public class Causes extends ParentActionBarPullToRefresh {
+public class Causes extends ParentFragment {
+
+    @Override
+    public int resId() {
+        return 0;
+    }
+
+    @Override
+    public void setUp(View v) {
+
+    }
 }
